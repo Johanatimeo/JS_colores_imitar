79552b0a54152d2960bcf88b08aa87e0970d3a52
Johana <leidyjohana.rincon@gmail.com>
2024-03-08 12:16:20 +0100

    feat: connect click event of button to change color logic

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function changeColor() {
-    const square = document.getElementByID('square')
+    const square = document.getElementById('square')
     square.classList.remove('red')
     square.classList.add('blue')
 }
+
+const button = document.getElementById('button')
+button.onclick = changeColor
